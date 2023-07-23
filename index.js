02557fbfc3e279b1b3ae458472d36083b054f2e0
ozgeonline <nisroc.414@hotmail.com>
2023-07-23 14:28:35 +0300

    Sticky Navbar
    sticky navbar that changes color with scroll

---
+++ index.js
@@ -0,0 +1,13 @@
+const navbarEl = document.querySelector(".navbar");
+const bottomContainerEl = document.querySelector(".bottom-container");
+
+// console.log(bottomContainerEl.offsetTop);
+// console.log(navbarEl.offsetHeight);
+
+window.addEventListener("scroll", () => {
+  if(window.scrollY > bottomContainerEl.offsetTop - navbarEl.offsetHeight - 40){
+    navbarEl.classList.add("active");
+  }else{
+    navbarEl.classList.remove("active");
+  }
+})
